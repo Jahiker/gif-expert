ff9f5a6cb6aab41774eb2e38bdf53fc5c5d84ab9
jahiker <rojasjahiker@gmail.com>
2023-01-08 00:26:10 -0400

    custom hooks

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGif";
+
+import { GifItem } from "./GifItem";
+
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  const { images, isLoading } = useFetchGifs( category );
 
   return (
     <>
       <h3>{category}</h3>
-      <ol>
-        {images.map(({ id, title }) => (
-          <li key={id}>{title}</li>
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifItem key={ image.id } { ...image }/>
         ))}
-      </ol>
+      </div>
     </>
   );
 };
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+import { getGifs } from "../helpers/getGif";
+
+export const useFetchGifs = ( category ) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  return { images, isLoading: false };
+};
