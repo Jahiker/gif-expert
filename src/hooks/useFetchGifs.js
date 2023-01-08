5ff7c4a1632e5864b8d070fafc248dfb5848a1fc
jahiker <rojasjahiker@gmail.com>
2023-01-08 01:32:24 -0400

    add readme

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -4,15 +4,18 @@ import { getGifs } from "../helpers/getGif";
 
 export const useFetchGifs = ( category ) => {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
 
   const getImages = async () => {
     const newImages = await getGifs(category);
+    
     setImages(newImages);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getImages();
   }, []);
 
-  return { images, isLoading: false };
+  return { images, isLoading };
 };
